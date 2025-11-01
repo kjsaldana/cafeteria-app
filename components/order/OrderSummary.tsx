4e45161e9faa6abe67319e2969d6ceb7ae3f3f94
Kevin J <kjoseph1910@hotmail.com>
2025-11-01 11:39:50 +0100

    feat: añadir ProductDetails.tsx e instalar @heroicons/react

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useStore } from "@/src/store"
+import ProductDetails from "./ProductDetails"
 
 export default function OrderSummary() {
   const order = useStore(state => state.order)
@@ -7,7 +8,12 @@ export default function OrderSummary() {
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
-      {order.length === 0 ? <p className="text-center my-10">El carrito esta vacio</p> : (<div className="mt-5"><p>Si hay algo</p></div>)}
+      {order.length === 0 ? <p className="text-center my-10">El carrito esta vacio</p> :
+        (<div className="mt-5">
+          {order.map(item => (
+            <ProductDetails key={item.id} item={item} />
+          ))}
+        </div>)}
     </aside>
   )
 }
