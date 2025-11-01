2bdf392c98d3fd7bbf4b448aa56bb62de8f00b66
Kevin J <kjoseph1910@hotmail.com>
2025-11-01 20:58:36 +0100

    feat: añadir decreaseQty y sus disable en ProductDetails.tsx

--- components/order/ProductDetails.tsx
+++ components/order/ProductDetails.tsx
@@ -2,6 +2,7 @@ import { useStore } from "@/src/store"
 import { OrderItem } from "@/src/types"
 import { formatCurrency } from "@/src/utils"
 import { MinusIcon, PlusIcon, XCircleIcon } from "@heroicons/react/24/outline"
+import { useMemo } from "react"
 
 type ProductDetailsProps = {
     item: OrderItem
@@ -9,13 +10,17 @@ type ProductDetailsProps = {
 
 export default function ProductDetails({ item }: ProductDetailsProps) {
     const increaseQty = useStore(state => state.increaseQty)
+    const decreaseQty = useStore(state => state.decreaseQty)
+    const disableIncreaseButton = useMemo(() => item.quantity === 10, [item])
+    const disableDecreaseButton = useMemo(() => item.quantity === 1, [item])
+
 
     return (
-        <div className="shadow space-y-1 p-4 bg-white  border-t border-gray-200 rounded-lg">
+        <div className="shadow space-y-1 p-4 bg-white border-b last-of-type:border-b-0">
             <div className="space-y-4">
                 <div className="flex justify-between items-start">
                     <p className="text-xl font-bold">{item.name} </p>
-                    <button type="button" onClick={() => { }}>
+                    <button type="button" onClick={() => {}}>
                         <XCircleIcon className="text-red-600 h-8 w-8 cursor-pointer" />
                     </button>
                 </div>
@@ -23,12 +28,22 @@ export default function ProductDetails({ item }: ProductDetailsProps) {
                 <p className="text-2xl text-amber-500 font-black">{formatCurrency(item.price)}</p>
 
                 <div className="flex gap-5 px-10 py-2 bg-gray-100 w-fit rounded-lg">
-                    <button type="button" onClick={() => { }}>
-                        <MinusIcon className="h-6 w-6 cursor-pointer" />
+                    <button
+                        type="button"
+                        onClick={() => decreaseQty(item.id)}
+                        className="cursor-pointer disabled:opacity-20 disabled:cursor-auto"
+                        disabled={disableDecreaseButton}
+                    >
+                        <MinusIcon className="h-6 w-6" />
                     </button>
                     <p className="text-lg font-black ">{item.quantity}</p>
-                    <button type="button" onClick={() => increaseQty(item.id)}>
-                        <PlusIcon className="h-6 w-6 cursor-pointer" />
+                    <button
+                        type="button"
+                        onClick={() => increaseQty(item.id)}
+                        className="cursor-pointer disabled:opacity-20 disabled:cursor-auto"
+                        disabled={disableIncreaseButton}
+                    >
+                        <PlusIcon className="h-6 w-6" />
                     </button>
                 </div>
 
