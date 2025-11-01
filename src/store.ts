254829150a324b9ae3d8ec31f70eca79fd8bb5e2
Kevin J <kjoseph1910@hotmail.com>
2025-11-01 11:17:43 +0100

    feat: terminar función addToOrder del store de Zustand

--- src/store.ts
+++ src/store.ts
@@ -7,9 +7,16 @@ interface Store {
     addToOrder: (product: Product) => void;
 }
 
-export const useStore = create<Store>(() => ({
+export const useStore = create<Store>((set) => ({
     order: [],
     addToOrder: (product) => {
-        console.log("agregando", product);
+        const { categoryId, image, ...data } = product
+        set((state) => ({
+            order: [...state.order, {
+                ...data,
+                quantity: 1,
+                subtotal: 1 * product.price
+            }]
+        }))
     }
 }))
