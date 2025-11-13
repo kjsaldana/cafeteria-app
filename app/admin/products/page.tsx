aa5f334e2f261e41b3a64c0bfb5a35c5299c23e0
Kevin J <kjoseph1910@hotmail.com>
2025-11-13 19:59:02 +0100

    feat: añadir getProductsWithCategory y ProductTable.tsx

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,8 +1,25 @@
+import ProductTable from "@/components/products/ProductTable";
 import Heading from "@/components/ui/Heading";
+import prisma from "@/src/lib/prisma"
 
-export default function productsPage() {
+async function getProducts() {
+    const products = await prisma.product.findMany({
+        include: {
+            category: true
+        }
+    })
+    return products
+}
+
+export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>
+
+export default async function productsPage() {
+    const products = await getProducts()
 
     return (
-        <Heading>Administrar Productos</Heading>
+        <>
+            <Heading>Administrar Productos</Heading>
+            <ProductTable products={products} />
+        </>
     )
 }
