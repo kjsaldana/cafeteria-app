e8b08353901ef3bb3b3d8c861d3334e8199bae4c
Kevin J <kjoseph1910@hotmail.com>
2025-11-25 19:45:57 +0100

    feat: actualizar Prisma client 7

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,28 +1,32 @@
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import 'dotenv/config';
 import { categories } from "./data/categories";
 import { products } from "./data/products";
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
     try {
         await prisma.category.createMany({
-            data: categories
-        })
+            data: categories,
+            skipDuplicates: true,
+        });
         await prisma.product.createMany({
-            data: products
-        })
+            data: products,
+            skipDuplicates: true,
+        });
+
+        console.log("Seed completed!");
     } catch (error) {
-        console.log(error);
+        console.error("Seed failed: ", error);
+    } finally {
+        await prisma.$disconnect();
     }
 }
 
-main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.log(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+main();
