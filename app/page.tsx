96770f686b7448bf98c7961076c386dce41014c8
Kevin J <kjoseph1910@hotmail.com>
2025-12-02 22:16:47 +0100

    feat: Añadir Ordenes Listas y su fetcher

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <>
-      <h1 className="text-4xl">Hola Mundo</h1>
-    </>
-  )
+  redirect('/order/cafe')
 }
