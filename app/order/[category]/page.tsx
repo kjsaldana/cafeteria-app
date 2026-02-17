8ea4460c9cea89cf6c1939788d46be9e94ef477e
Kevin J <kjoseph1910@hotmail.com>
2026-02-17 19:57:52 +0100

    proyecto importado de nextjs 15 hacia 16

---
+++ app/order/[category]/page.tsx
@@ -0,0 +1,34 @@
+import ProductCard from "@/components/products/ProductCard"
+import prisma from "@/src/lib/prisma"
+
+type OrderPageProps = {
+    params: Promise<{ category: string }>
+}
+
+async function getProducts(category: string) {
+    const products = await prisma.product.findMany({
+        where: {
+            category: {
+                slug: category
+            }
+        }
+    })
+    return products
+}
+
+export default async function OrderPage({ params }: OrderPageProps) {
+    const { category } = await params
+    const products = await getProducts(category)
+
+    return (
+        <>
+            <h1 className="text-2xl my-10">Elige y personaliza tu pedido a continuación</h1>
+            
+            <div className="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4 gap-4 items-start">
+                {products.map(product => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
+        </>
+    )
+}
---
+++ app/products/new/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function createProductPage() {
+    return (
+        <div>createProductPage</div>
+    )
+}
