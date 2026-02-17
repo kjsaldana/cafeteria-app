fb3827c262643745eb3c164b9d391b7f2c622646
Kevin J <kjoseph1910@hotmail.com>
2026-02-17 19:58:01 +0100

    categorias resaltadas y useParams() añadido

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,14 +1,19 @@
-import { Category } from "@/app/generated/prisma"
+"use client"
+import { Category } from "@/app/generated/prisma/client"
 import Image from "next/image"
 import Link from "next/link"
+import { useParams } from "next/navigation"
 
 type CategoryIconProps = {
   category: Category
 }
 
 export default function CategoryIcon({ category }: CategoryIconProps) {
+  const params = useParams<{ category: string }>()
+  const isSelected = category.slug === params.category && "bg-amber-400"
+
   return (
-    <div className="flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b">
+    <div className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b ${isSelected}`}>
       <div className="w-16 h-16 relative">
         <Image fill src={`/icon_${category.slug}.svg`} alt="Imagen categoria" />
       </div>
